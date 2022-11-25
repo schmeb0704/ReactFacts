a80a2b1bf6e690e6e918933d3b7ec68b79c28d08
schmeb0704 <113418470+schmeb0704@users.noreply.github.com>
2022-11-25 07:34:44 +0000

    copied the codebase from scrimba

---
+++ themed-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+
+export default ()=>{
+  return(
+    <>
+      <div className="container">
+        <Navbar />
+        <Main />
+      </div>
+    </>
+  )
+}

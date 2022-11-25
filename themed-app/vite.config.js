773f5b870f748b82a5d084e92ec3e0c06ee45929
schmeb0704 <113418470+schmeb0704@users.noreply.github.com>
2022-11-25 12:21:33 +0000

    kapoy na

--- themed-app/vite.config.js
+++ themed-app/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ReactFacts/",
   plugins: [react()],
+  base: "/ReactFacts/",
   build:{
     outDir: "docs"
   }
